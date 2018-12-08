808c15ee1f9cbb574a0cfdddf9aa96360358460e
jeongjoonsup <penny4860@gmail.com>
2018-12-08 16:27:15 +0900

    Updates

---
+++ precache-manifest.01f42fd1d384d031292b41344f303525.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "95e4f34253c24166573f",
+    "url": "/myapp/static/js/main.95e4f342.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/myapp/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "95e4f34253c24166573f",
+    "url": "/myapp/static/css/main.ead40868.chunk.css"
+  },
+  {
+    "revision": "f6e149cca4b2ba7873bb40d0eddeea38",
+    "url": "/myapp/index.html"
+  }
+];
